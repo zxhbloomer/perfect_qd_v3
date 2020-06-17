81fbe7d99918e21b248cb041d7c82b5d492b2836
zxhbloomer <zxhbloomer@gmail.com>
2020-06-17 21:21:43 +0800

    ..

---
+++ src/components/InputSelect/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-input
+    v-model.trim="value"
+    disabled
+    :placeholder="isPlaceholderShow('请选择')"
+  >
+    <el-button
+      slot="append"
+      icon="el-icon-search"
+      :disabled="isViewModel"
+      @click="handleStaffDialogClickTwo"
+    >
+      选择
+    </el-button>
+  </el-input>
+</template>
+
+<script>
+export default {
+  name: 'InputSelect',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    selectVal: {
+      get() {
+        return [...this.value]
+      },
+      set(val) {
+        this.$emit('input', [...val])
+      }
+    }
+  },
+  mounted() {
+    this.setSort()
+  },
+  methods: {
+
+  }
+}
+</script>
